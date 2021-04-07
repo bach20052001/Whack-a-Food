da507da14c278cca8643b77ad5a97c70141e71e0
DESKTOP-742FDTC\Admin <bach20501@gmail.com>
2021-04-07 16:46:04 +0700

    initial commit

---
+++ Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+
+}
---
+++ Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonX : MonoBehaviour
+{
+    private Button button;
+    private GameManagerX gameManagerX;
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    /* When a button is clicked, call the StartGame() method
+     * and pass it the difficulty value (1, 2, 3) from the button 
+    */
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked");
+        switch (button.gameObject.name)
+        {
+            case "Easy":
+                difficulty = 1;
+                break;
+            case "Medium":
+                difficulty = 2;
+                break;
+            case "Hard":
+                difficulty = 3;
+                break;
+            default:
+                break;
+        }
+        gameManagerX.StartGame(difficulty);
+    }
+
+
+
+}
